48e0027438ac239f909e2988ea835a996d3f5ca1
Arman <arman.takhani@elev.ga.ntig.se>
2024-10-22 10:56:13 +0200

    skapat oython fil och Readme fil

---
+++ halloween.py
@@ -0,0 +1,11 @@
+karaktar = input("Vilken är din favorit-djur-varelse? (apa, häst, flodhäst, val): ").lower()
+
+    
+if karaktar == "apa":
+    print("Här är en banan!")
+elif karaktar == "häst":
+    print("Här är ett äpple!")
+elif karaktar == "flodhäst":
+    print("Här är lite vattenmelon!")
+else:
+    print("Va kul karaktär du har fått")
